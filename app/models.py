8dc40a85d0c6b4bf0f1ab76b0ca79662b820e499
Rea <rmrea@kenya-airways.com>
2022-01-31 10:30:32 +0300

    add articles class in models

--- app/models.py
+++ app/models.py
@@ -10,3 +10,17 @@ class Sources:
         self.category = category
         self.country = country
         self.language = language
+
+
+class Articles:
+    '''
+    This class will define articles objects
+    '''
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
