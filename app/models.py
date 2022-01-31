e18c60eecea39cd2ff1fc0d3342d89389372023b
Rea <rmrea@kenya-airways.com>
2022-01-31 10:28:21 +0300

    add news source class

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Sources:
+    '''
+    This class defines news Sources
+    '''
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
