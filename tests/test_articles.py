2ad25727183816fc333b11c6a8ad1b92ab5fd6d9
Rea <rmrea@kenya-airways.com>
2022-01-31 22:24:13 +0300

    add tests for Articles class

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -10,16 +10,16 @@ class ArticlesTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_article = Articles('bloomberg','Max Reyes','Deutsche Bank Staff Back in NYC Offices as Covid Cases Decline','Deutsche Bank AG employees returned to the lender’s U.S. headquarters in New York','assets.bwbx.io','assets.bwbx.io/is0woRr8jhN4.jpg','2022-01-31T17:36:33Z')
+        self.new_article = Articles('bloomberg','Max Reyes','Deutsche Bank Staff Back in NYC Offices as Covid Cases Decline','Deutsche Bank AG employees returned to the lenders U.S. headquarters in New York','assets.bwbx.io','assets.bwbx.io/is0woRr8jhN4.jpg','2022-01-31T17:36:33Z')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Articles))
 
     def test_to_check_instance_variables(self):
-        self.assertEquals(self.new_article.id,'CNN')
-        self.assertEquals(self.new_article.author,'Peter Polle')
-        self.assertEquals(self.new_article.title,'The tech scene in Africa-Is it the next big thing?')
-        self.assertEquals(self.new_article.description,'A look at various tech hubs in Africa and the impact they have on the worlds economy')
-        self.assertEquals(self.new_article.url,'techie.com')
-        self.assertEquals(self.new_article.image,'techie.com/7643t94.jpg')
-        self.assertEquals(self.new_article.date,'2018-04-11T07:57:16Z')
+        self.assertEquals(self.new_article.id,'bloomberg')
+        self.assertEquals(self.new_article.author,'Max Reyes')
+        self.assertEquals(self.new_article.title,'Deutsche Bank Staff Back in NYC Offices as Covid Cases Decline')
+        self.assertEquals(self.new_article.description,'Deutsche Bank AG employees returned to the lenders U.S. headquarters in New York')
+        self.assertEquals(self.new_article.url,'assets.bwbx.io')
+        self.assertEquals(self.new_article.image,'assets.bwbx.io/is0woRr8jhN4.jpg')
+        self.assertEquals(self.new_article.date,'2022-01-31T17:36:33Z')
