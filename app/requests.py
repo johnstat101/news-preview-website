e39c0de405ded76a38ac245baef5df95b3b22bc4
Rea <rmrea@kenya-airways.com>
2022-01-31 13:08:17 +0300

    add function to obtain response to url requests

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 import urllib.request, json
 from .models import Sources, Articles
 
@@ -7,7 +8,25 @@ api_key = None
 base_url = None
 
 def configure_request(app):
+    '''
+    function to configure requests
+    '''
     global api_key, base_url,articles_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_SOURCES_BASE_URL']
     articles_url = app.config['ARTICLES_BASE_URL']
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category, api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results)
